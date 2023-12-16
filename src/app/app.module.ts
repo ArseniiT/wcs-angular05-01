70b4b35a56e30e5a60325dabacfb211ae80965ad
arseniit <arseniit@gmail.com>
2023-12-17 00:19:16 +0100

    Add form validation and status messaging

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,15 +4,19 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { SignUpComponent } from './sign-up/sign-up.component';
+import { HomeComponent } from './home/home.component';
+import { FormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [
     AppComponent,
-    SignUpComponent
+    SignUpComponent,
+    HomeComponent,
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    FormsModule,
+    AppRoutingModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/sign-up/sign-up.component.ts
+++ src/app/sign-up/sign-up.component.ts
@@ -6,5 +6,27 @@ import { Component } from '@angular/core';
   styleUrls: ['./sign-up.component.css']
 })
 export class SignUpComponent {
+  user: User;
 
+  constructor() {
+    this.user = new User();
+  }
+
+  onSubmit() {
+    console.log(this.user);
+  }
+  change(e: any) {
+    console.log(e);
+  }
+
+
+}
+
+class User {
+  constructor(
+    public firstname?: string,
+    public lastname?: string,
+    public email?: string,
+    public password?: string
+  ){}
 }
